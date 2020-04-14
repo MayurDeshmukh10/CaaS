74a7f00ab0e1aa797e69c26eb24b7dd4d894d76f
Mayur Deshmukh <mayur.s.deshmukh10@gmail.com>
2020-04-14 21:40:12 +0530

    Some changes

--- __init__.py
+++ __init__.py
@@ -3,3 +3,5 @@ from CaaS.caas import text_decompression
 from CaaS.caas import audio_compression
 from CaaS.caas import image_compression
 from CaaS.caas import video_compression
+from CaaS.errors import Error
+from Caas.errors import InvalidAuthKeyError
